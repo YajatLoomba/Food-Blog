a1bd6223321209cd92425e0300e00bc36ee7ef1a
YajatLoomba <yajatloomba2000@gmail.com>
2021-07-04 15:53:53 +0530

    update1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express") 
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
+const port = process.env.PORT || 3000;
 
 const app = express()
 
@@ -45,7 +46,9 @@ app.get("/",(req,res)=>{
         "Allow-access-Allow-Origin": '*'
     })
     return res.redirect('index.html');
-}).listen(3000);
+}).listen(port, () =>{
 
-console.log("Listening on PORT 3000")
+
+console.log("Listening on PORT no at ${port}");
+}) 
  
